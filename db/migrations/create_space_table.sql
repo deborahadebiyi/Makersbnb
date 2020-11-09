9a2f89774d97870cccf9c05393f8d5a516321e00
Christian Naykene <younchriss@live.co.uk>
2020-11-09 16:39:39 +0000

    Added table command

---
+++ db/migrations/create_space_table.sql
@@ -0,0 +1,1 @@
+CREATE TABLE space(id SERIAL PRIMARY KEY, name VARCHAR(60), description VARCHAR(200), price NUMERIC(5, 2), date DATE, availability VARCHAR(60), FOREIGN KEY (user_id) REFERENCES users(id)
