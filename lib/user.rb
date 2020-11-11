59541fd662518054b38a9ef5fd9bad710ed0f9bc
swizel <swizel.sarah@gmail.com>
2020-11-11 11:49:36 +0000

    database connection

--- lib/user.rb
+++ lib/user.rb
@@ -1,4 +1,6 @@
 require 'pg'
+require_relative 'database_connection'
+
 
 class User
   attr_reader :id,:username
@@ -11,16 +13,14 @@ class User
 
   def self.sign_up(username:, password:)
     return false if self.name_taken(username: username)
-    connection = self.connect
-    connection.exec("INSERT INTO users (username,password) VALUES ('#{username}','#{password}')")
+    DatabaseConnection.query("INSERT INTO users (username,password) VALUES ('#{username}','#{password}')")
     self.log_in(username: username, password: password)
     return true
   end
 
   def self.log_in(username:, password:)
     return false if !self.name_taken(username: username)
-    connection = self.connect
-    result = connection.exec("SELECT * FROM users WHERE username LIKE '#{username}' AND password LIKE '#{password}'")
+    result = DatabaseConnection.query("SELECT * FROM users WHERE username LIKE '#{username}' AND password LIKE '#{password}'")
     if result.ntuples != 0
       @current_user = result[0]['id']
       true
@@ -30,16 +30,14 @@ class User
   end
 
   def self.all
-    connection = self.connect
-    result = connection.exec("SELECT * FROM users")
+    result = DatabaseConnection.query("SELECT * FROM users")
     result.map do |user|
       User.new(user['id'],user['username'])
     end
   end
   
   def self.find_user(id:)
-    connection = self.connect
-    result = connection.exec("SELECT * FROM users WHERE id = '#{id}'")
+    result = DatabaseConnection.query("SELECT * FROM users WHERE id = '#{id}'")
     User.new(result[0]['id'], result[0]['username'])
   end
 
@@ -50,7 +48,6 @@ class User
   private
 
   def self.name_taken(username:)
-    connection = self.connect
     result = self.all
     result.each do |user|
       return true if user.username == username
@@ -58,11 +55,4 @@ class User
     return false
   end
 
-  def self.connect
-    if ENV["ENVIRONMENT"] == 'test'
-      connection = PG.connect(dbname: 'makersbnb_test')
-    else
-      connection = PG.connect(dbname: 'makersbnb')
-    end
-  end
 end
