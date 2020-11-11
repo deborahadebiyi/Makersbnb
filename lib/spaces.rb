0046b1dbfdb30dbc43206b00d9a9723e9f4ad687
swizel <swizel.sarah@gmail.com>
2020-11-11 14:08:18 +0000

    methos added to spaces

--- lib/spaces.rb
+++ lib/spaces.rb
@@ -1,42 +1,44 @@
 require 'pg'
+require_relative 'database_connection'
 
 class Space
 
-  attr_reader :name, :description, :price, :date, :availability
+  attr_reader :name, :description, :price, startdate:, enddate:,:availability, :user_id
 
-  def initialize(name:, description:, price:, date:, availability:)
+  def initialize(id:, name:, description:, price:, startdate:, enddate:, availability:, user_id:)
+    @id = id
     @name = name
     @description = description
     @price = price
-    @date = date
+    @startdate = startdate
+    @enddate = enddate
     @availability = availability
+    @user_id = user_id
 
   end
 
   def self.all
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'makersbnb_test')
-    else
-      connection = PG.connect(dbname: 'makersbnb')
-    end
-
-    result = connection.exec('SELECT * FROM spaces')
-    result.map { |space| space['name']}
+    result = DatabaseConnection.query('SELECT * FROM spaces')
+    result.map do |space| 
+        Space.new(result[0]['id'], result[0]['name'], result[0]['description'], result[0]['price'], result[0]['startdate'], result[0]['enddate'], result[0]['availability'], result[0]['user_id'])
+    end 
 
   end
 
-  def self.create_space(name:, description:, price:, date:, availability:)
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'makersbnb_test')
-    else
-      connection = PG.connect(dbname: 'makersbnb')
-    end
-
-    connection.exec("INSERT INTO spaces(name, description, price, date, availability) VALUES('#{name}', '#{description}', '#{price}', '#{date}', '#{availability}')")
+  def self.create_space(name:, description:, price:, startdate:, enddate:, availability:)
+    DatabaseConnection.query("INSERT INTO spaces(name, description, price, startdate:, enddate:, availability) 
+    VALUES('#{name}', '#{description}', '#{price}', '#{startdate}', '#{enddate}','#{availability}')")
   end
 #refactor to create_space
 
-  def self.is_available?
+  def self.is_available(id:)
+    result = DatabaseConnection.query("SELECT * FROM spaces WHERE id = '#{id}'")
+    return true if result[0]['availability'] == true
+    return false 
+  end
 
+  def self.find_space(id:)
+    result = DatabaseConnection.query("SELECT * FROM users WHERE id = '#{id}'")
+    Space.new(result[0]['id'], result[0]['name'], result[0]['description'], result[0]['price'], result[0]['startdate'], result[0]['enddate'], result[0]['availability'], result[0]['user_id'])
   end
 end
