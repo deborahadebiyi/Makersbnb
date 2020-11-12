5422a81db0bc154da367ddb2b13158e2cd66150a
deborah adebiyi <deborahadebiyi@hotmail.co.uk>
2020-11-12 14:13:16 +0000

    views fix

--- lib/bookings.rb
+++ lib/bookings.rb
@@ -1,28 +1,30 @@
 class Booking
-    attr_reader :id, :space_id, :host_id, :user_id, :approval
+    attr_reader :booking_id, :space_id, :host_id, :user_id, :approval
 
-    def initialize(id, space_id, host_id, user_id, approval)
-        @id = id
+    def initialize(booking_id, space_id, host_id, user_id, approval)
+        @booking_id = id
         @space_id = space_id
         @host_id = host_id
         @user_id = user_id
         @approval = approval
-    end 
+    end
 
     def self.approve(booking_id:)
         DatabaseConnection.query("UPDATE bookings SET approval = true WHERE booking_id = #{booking_id}")
         return true
-    end 
-  
+    end
+
     def self.check_bookings(user_id:)
         result = DatabaseConnection.query("SELECT * FROM bookings WHERE user_id = #{user_id}")
         result.map do |booking|
-            Booking.new(booking['id'], booking['space_id'], booking['host_id'], booking['user_id'], booking['approval'])
+          if booking['approval'] == true
+            Booking.new(booking['booking_id'], booking['space_id'], booking['host_id'], booking['user_id'], booking['approval'])
+          end
         end
-    end 
+    end
 
     def self.find_booking_id(space_id:)
         result = DatabaseConnection.query("SELECT * FROM bookings WHERE space_id = #{space_id}")
         return result[0]['booking_id']
-    end 
-end 
+    end
+end
